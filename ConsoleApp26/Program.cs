3230c44c0f6134382020231f53f4bdd3ecb7fce1
Ali Aliyev <elvinaliyev724@gmail.com>
2024-04-10 20:10:02 +0400

    finish

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ali = new Student();
+            Console.WriteLine(ali.Age1);
+
+            Student murad = new Student();
+            Console.WriteLine(murad.Age1);
+
+            Student gadir = new Student();
+            Console.WriteLine(gadir.Age1);
+
+            Student nicat = new Student();
+            Console.WriteLine(nicat.Age1);
+        }
+
+        public class Student
+        {
+            private static int Age = 0;
+            public int Age1 { get; set; }
+
+            public Student()
+            {
+                Age1 = ++Age;
+            }
+        }
+    }
+}
